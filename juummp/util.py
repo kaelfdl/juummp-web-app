6e583cf14ed6b53b4bf799ffb54e932e52f878fa
Mikael Gabryel Flor de Lis <gabryel.flordelis@gmail.com>
2023-07-11 16:36:32 +0400

    Added boto3 session

--- juummp/util.py
+++ juummp/util.py
@@ -1,7 +1,8 @@
 import boto3
 
 def get_aws_parameters(store, params):
-    client = boto3.client('ssm')
+    session = boto3.Session()
+    client = boto3.client('ssm', region_name='eu-west-1')
     response = client.get_parameters(Names=params, WithDecryption=True)
     for n in range(len(response['Parameters'])):
         store[response['Parameters'][n]['Name']] = response['Parameters'][n]['Value']
